3eeacdb1ec4c151a4242f1eec0d26c854ca18251
BenCree <benstuartcree@gmail.com>
2024-04-02 04:15:05 +0100

    finished state.to_array(), returns board position + castling & en passant 1hot

---
+++ board_state.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import torch
+import chess
+import chess.pgn
+import numpy as np
+import re
+
+
+class state():
+    '''wrapper around chess.board'''
+    def __init__(self, board):
+        if board is None:
+            self.board = chess.Board()
+        else:
+            self.board = board
+        #print(board)
+    def get_fen(self):
+        fen = board.fen()
+        return fen
+    @staticmethod
+    def parse_fen(fen):
+        fen = fen.split(' ')
+        castling_array = np.zeros(4, np.uint16)
+        enpassant_array = np.zeros(1, np.uint16)
+        castle_fen = fen[2]
+        enpassant_sq = fen[3]
+        castling_key = {'K': 0, 'Q': 1, 'k': 2, 'q': 3}
+
+        for state in castle_fen:
+            if state in castling_key:
+                castling_array[castling_key[state]] = 1
+        if enpassant_sq != '-':
+            enpassant_array[0] = 1
+        fen_array = np.concatenate((castling_array, enpassant_array), axis=0)
+        # print(f'castling state {castle_fen}')
+        # print(f'castling array {castling_array}')
+        # print(f'fen array {fen_array}')
+        return fen_array
+    def to_array(self):
+        '''64 bit array '''
+        # piece_dict = {'R': 1, 'N': 2, 'B': 3, 'K': 4, 'Q': 5, \
+        #               'r': 6, 'n': 7, 'b': 8, 'k': 9, 'q': 10}
+        board = self.board
+        state_array = np.zeros(64, np.uint16)
+        fen = board.fen()
+        fen = fen.split(' ')
+        for i in range(64):
+            if board.piece_at(i) is not None:
+                state_array[i] = board.piece_at(i).piece_type
+            else:
+                state_array[i] = 0
+        #print(state_array)
+        castling_array = np.zeros(4, np.uint16)
+        enpassant_array = np.zeros(1, np.uint16)
+        castle_fen = fen[2]
+        enpassant_sq = fen[3]
+        castling_key = {'K': 0, 'Q': 1, 'k': 2, 'q': 3}
+
+        for state in castle_fen:
+            if state in castling_key:
+                castling_array[castling_key[state]] = 1
+        if enpassant_sq != '-':
+            enpassant_array[0] = 1
+        cstl_npsnt_array = np.concatenate((castling_array, enpassant_array), axis=0)
+        full_array = np.concatenate((state_array, cstl_npsnt_array), axis=0)
+        # print(f'castling state {castle_fen}')
+        # print(f'castling array {castling_array}')
+        # print(f'fen array {fen_array}')
+        return full_array
+
+        return state_array
--- parse_data.py
+++ parse_data.py
@@ -4,38 +4,16 @@ import chess
 import chess.pgn
 import numpy as np
 import re
+from board_state import state
 pgn = open('ficsgamesdb_2023_standard2000_nomovetimes_330240.pgn')
 
-class state():
-    '''wrapper around chess.board'''
-    def __init__(self, board):
-        if board is None:
-            self.board = chess.Board()
-        else:
-            self.board = board
-        #print(board)
-
-    def parse_fen(self, fen):
-        self.fen = fen
-        return fen
-    def to_array(self):
-        '''64 bit array '''
-        # piece_dict = {'R': 1, 'N': 2, 'B': 3, 'K': 4, 'Q': 5, \
-        #               'r': 6, 'n': 7, 'b': 8, 'k': 9, 'q': 10}
-        state_array = np.zeros(64, np.uint16)
-        fen = board.fen()
-        for i in range(64):
-            if board.piece_at(i) is not None:
-                state_array[i] = board.piece_at(i).piece_type
-            else:
-                state_array[i] = 0
-        #print(state_array)
-        return state_array
-        # TODO: add flags for castling, en passant, and anything else that isnt a basic board position
 
 game_num = 0
 dat = []
 res_dict = {'1-0':1,'0-1':0,'1/2-1/2':0.5}
+test_fen = '4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1'
+
+state.parse_fen(fen=test_fen)
 while True:
 
 
@@ -56,16 +34,21 @@ while True:
             #print(f'fen {board.fen()}')
             board_state = state(board)
             board_array = board_state.to_array()
+
             states.append([board_array, res_dict[res]])
             #print(array)
             board.push(move)
         #print(states)
         assert int(game.headers['PlyCount']) == len(states)
         dat.append(states)
+#
+#print(f'training dat: {dat}')
 
-print(f'training dat: {dat}')
-for d in dat:
-    print(len(d))
+for game in dat[-1]:
+    print(len(game))
+    for move in game:
+     print(move)
+
 
 
 
